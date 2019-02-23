172bea0b7d236e140e9583ef4277e4de84f8d388
vijaya chevakula <vchevakula@gmail.com>
2019-02-23 15:21:07 -0500

    modified file

--- file_open_write.py
+++ file_open_write.py
@@ -5,3 +5,5 @@ with open('/Users/vchevakula/python/test2.txt','r',encoding = 'utf-8') as f:
     #f.write('USCIS\n')
     #f.seek(4)
     #f.write('I have applied for my H1B')
+    for f in f_contents:
+	print(f)
