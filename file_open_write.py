760eb96781b99bb3d71eb392189410ff962778b7
vijaya chevakula <vchevakula@gmail.com>
2019-02-22 22:11:18 -0500

    second commit

---
+++ file_open_write.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3
+with open('/Users/vchevakula/python/test2.txt','r',encoding = 'utf-8') as f:
+    f_contents = f.read()
+    print (f_contents)
+    #f.write('USCIS\n')
+    #f.seek(4)
+    #f.write('I have applied for my H1B')
